5d3636e984700745d91ad67ac1382566bd9ac21c
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2021-12-19 14:53:10 +0100

    Started with AASX File Service implementation

---
+++ src/aas-aasxfile-service/AASAASXFile.cs
@@ -0,0 +1,40 @@
+﻿using AAS.API.Models.Interfaces;
+using AAS.API.Services;
+using System;
+
+namespace AAS.API.AASXFile
+{
+    public interface AASAASXFile : AASXFileServer
+    {
+    }
+    public class AASXFileServiceException : AASServiceException
+    {
+        //
+        // Summary:
+        //     Initializes a new instance of the Azure.RequestFailedException class with a specified
+        //     error message.
+        //
+        // Parameters:
+        //   message:
+        //     The message that describes the error.
+        public AASXFileServiceException(string message) : base(message)
+        {
+        }
+        //
+        // Summary:
+        //     Initializes a new instance of the Azure.RequestFailedException class with a specified
+        //     error message, HTTP status code and a reference to the inner exception that is
+        //     the cause of this exception.
+        //
+        // Parameters:
+        //   message:
+        //     The error message that explains the reason for the exception.
+        //
+        //   innerException:
+        //     The exception that is the cause of the current exception, or a null reference
+        //     (Nothing in Visual Basic) if no inner exception is specified.
+        public AASXFileServiceException(string message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/aas-aasxfile-service/AzureBlobImpl/AzureBlobAASXFileService.cs
@@ -0,0 +1,49 @@
+﻿using AAS.API.Models;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAS.API.AASXFile
+{
+    public class AzureBlobAASXFileService : AASAASXFile
+    {
+        private BlobServiceClient blobServiceClient; // See https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-dotnet#upload-a-blob-to-a-container
+
+        private readonly ILogger _logger;
+
+        public AzureBlobAASXFileService(BlobServiceClient serviceClient, IConfiguration config, ILogger<AzureBlobAASXFileService> logger)
+        {
+            blobServiceClient = serviceClient;
+            _logger = logger;
+        }
+
+        public async Task<PackageDescription> CreateAASXPackage(List<string> aasIds, byte[] file, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAASXByPackageId(string packageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<byte[]> GetAASXByPackageId(string packageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<PackageDescription>> GetAllAASXPackageIds(string aasId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PackageDescription> UpdateAASXPackage(string packageId, List<string> aasIds, byte[] file, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
