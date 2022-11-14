4d0d55df5fa4c89ac5cfa5416960eb36f25aaaa3
Marcus Schüle <mse@mm-software.com>
2022-11-14 11:08:26 +0100

    corrected and improved existing

--- scripts/azuredeployment/createAzureDigitalTwinsInstance.ps1
+++ scripts/azuredeployment/createAzureDigitalTwinsInstance.ps1
@@ -7,16 +7,28 @@ param (
 	[string] $dtName = "aasrepositorydigitaltwins"
 )
 
+Write-Host "`nStarting Deployment of Azure Digital Twins" $dtName "`n"
+
 # 1. Check existenc of resource group and create it if it's not existing yet
 $rsgExists = az group exists -n $rg
 if ( $rsgExists -eq 'false') {
 	Write-Host "Resource group " $rg " doesn't exist. Creating it now."
 	az group create -l $dcloc -n $rg
 }
+else {
+	Write-Host "Using existing Resource Group " $rg
+}
 
 # 2. Create Azure Digital Twin instance
-az dt create --dt-name $dtName -g $rg -l $dcloc --assign-identity true
+$digitalTwinsExists = $(az dt list -g $rg --query "[?name=='$dtName']") | ConvertFrom-Json
+if ($digitalTwinsExists.Length -eq 0 ) {
+	az dt create --dt-name $dtName -g $rg -l $dcloc --assign-identity true
+	Write-Host "Created Azure Digital Twins Instance " $dtName
 
-# 3. Create ADT Data Owner role for current user
-$currAcc = $(az account show) | ConvertFrom-Json
-az dt role-assignment create --dt-name $dtName -g $rg --assignee $currAcc.user.name --role "Azure Digital Twins Data Owner"
+	# 3. Create ADT Data Owner role for current user
+	$currAcc = $(az account show) | ConvertFrom-Json
+	az dt role-assignment create --dt-name $dtName -g $rg --assignee $currAcc.user.name --role "Azure Digital Twins Data Owner"
+}
+else {
+	Write-Host "Digital Twins Instance " $dtName "already exists"
+}
--- scripts/azuredeployment/createWebApiAppForService.ps1
+++ scripts/azuredeployment/createWebApiAppForService.ps1
@@ -9,26 +9,42 @@ param (
 	[string] $appName = "aasapiservice"
 )
 
+Write-Host "`nStarting Deployment of AASWebApi" $appName "`n"
+
 # 1. Check existenc of resource group and create it if it's not existing yet
 $rsgExists = az group exists -n $rg
 if ( $rsgExists -eq 'false') {
 	Write-Host "Resource group " $rg " doesn't exist. Creating it now."
 	az group create -l $dcloc -n $rg
 }
+else {
+	Write-Host "Using existing Ressource Group" $rg
+}
 
 # 2. Create App service plan if it doesn't exist yet
-$existingPlan=$(az appservice plan list -g $rg --query "[?name=='$planName']")
-if ($existingPlan.length == 0) {
-	az appservice plan create --name $planName --g $rg --location $dcloc --is-linux --sku $planSku
+$existingPlans=$(az appservice plan list -g $rg --query "[?name=='$planName']") | ConvertFrom-Json
+if ($existingPlans.Length -eq 0) {
+	Write-Host "Creating App Service Plan " $planName
+	az appservice plan create --name $planName -g $rg --location $dcloc --is-linux --sku $planSku
+}
+else {
+	Write-Host "Using existing App Service Plan " $planName
 }
 
 # 3. Create Web app
-az webapp create -g $rg -p $planName -n $appName --runtime "DOTNETCORE|3.1" --assign-identity
+$existingWebApps=$(az webapp list -g $rg --query "[?name=='$planName']") | ConvertFrom-Json
+if ($existingWebApps.Length -eq 0) {
+	Write-Host "Creating Web App" $appName
+	az webapp create -g $rg -p $planName -n $appName --runtime "DOTNETCORE:3.1" --assign-identity
+	# 4. Create App registration for Web app
+	$clientId = $(az ad app create --display-name $appName --sign-in-audience 'AzureADMyOrg'  --app-roles '@aasapiservicestdroles.json' --query appId -o tsv)
 
+	# 5. Set Azure AD configurtion in Web app
+	az webapp config appsettings set --name $appName -g $rg --settings "AzureAd__ClientId=$clientId"
+}
+else {
+	Write-Host "WebApp" $appName "already exists"
+}
 # TODO Set 'kind' to "linux,api" with 'az resource update' to enable API features in portal
 
-# 4. Create App registration for Web app
-$clientId = $(az ad app create --display-name $appName --available-to-other-tenants false --app-roles @aasapiservicestdroles.json --query appId -o tsv)
 
-# 5. Set Azure AD configurtion in Web app
-az webapp config appsettings set --name $appName -g $rg --settings "AzureAd__ClientId=$clientId"
