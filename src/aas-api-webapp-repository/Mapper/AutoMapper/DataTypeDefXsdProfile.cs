2fb0c4686a04e5d89940f424e522423906380c17
Marcus Schüle <mse@mm-software.com>
2023-01-11 09:36:41 +0100

    added JObjectToProperty Mapper + extensionmethods

---
+++ src/aas-api-webapp-repository/Mapper/AutoMapper/DataTypeDefXsdProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using AasCore.Aas3_0_RC02;
+using AutoMapper;
+
+namespace Aas.Api.Repository.Mapper.AutoMapper
+{
+
+    public class DataTypeDefXsdProfile :  Profile
+    {
+        public DataTypeDefXsdProfile()
+        {
+            CreateMap<string, DataTypeDefXsd>()
+                .ConvertUsing(new StringToDataTypeDefXsdConverter());
+        }
+    }
+
+    public class StringToDataTypeDefXsdConverter : ITypeConverter<string, DataTypeDefXsd>
+    {
+        public DataTypeDefXsd Convert(string source, DataTypeDefXsd destination, ResolutionContext context)
+        {
+            var asdf = source.StartsWith("xs:", StringComparison.InvariantCultureIgnoreCase) 
+                ? source[3..source.Length] 
+                : source;
+            var result = Enum.Parse<DataTypeDefXsd>(asdf, true);
+            return result;
+        }
+    }
+}
---
+++ src/aas-api-webapp-repository/Mapper/JObjectToAasObjectMapperStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Aas.Api.Repository.Mapper
+{
+    public interface ObjectMapperStrategy<T>
+    {
+        public T map(JObject obj);
+    }
+}
