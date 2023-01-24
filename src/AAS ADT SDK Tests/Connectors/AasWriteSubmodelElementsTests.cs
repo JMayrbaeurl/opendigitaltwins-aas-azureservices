1bcd5bbe6ba787187590d2e227ad1bc9159029db
Marcus Schüle <mse@mm-software.com>
2023-01-24 14:21:45 +0100

    Added AasWriteSubmodelElements

---
+++ src/AAS ADT SDK Tests/Connectors/AasWriteSubmodelElementsTests.cs
@@ -0,0 +1,210 @@
+﻿using AasCore.Aas3_0_RC02;
+using Azure.DigitalTwins.Core;
+using Microsoft.Extensions.Logging;
+using Moq;
+using File = AasCore.Aas3_0_RC02.File;
+
+namespace AAS.ADT.Tests
+{
+    [TestClass]
+    public class AasWriteSubmodelElementsTests
+    {
+        private AasWriteSubmodelElements _objectUnderTest;
+        private Mock<ILogger<AasWriteSubmodelElements>> _loggerMock;
+        private Mock<IAdtTwinFactory> _adtTwinFactoryMock;
+        private Mock<IAasWriteConnector> _writeConnectorMock;
+        private Mock<IAasWriteBase> _writeBaseMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<AasWriteSubmodelElements>>();
+            _writeConnectorMock = new Mock<IAasWriteConnector>();
+            _adtTwinFactoryMock = new Mock<IAdtTwinFactory>();
+            _writeBaseMock = new Mock<IAasWriteBase>();
+
+            _adtTwinFactoryMock.Setup(_ => _.GetTwin(It.IsAny<ISubmodelElement>())).Returns(
+                new BasicDigitalTwin
+                {
+                    Id = "testSubmodelElementTwinId"
+                });
+
+            _objectUnderTest = new AasWriteSubmodelElements(_loggerMock.Object, _adtTwinFactoryMock.Object,
+                _writeConnectorMock.Object, _writeBaseMock.Object);
+
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_Creates_Property()
+        {
+            var minimalProperty = new Property(DataTypeDefXsd.Boolean);
+            await _objectUnderTest.CreateSubmodelElement(minimalProperty);
+
+            _adtTwinFactoryMock.Verify(_ => _.GetTwin(It.IsAny<Property>()), Times.Once());
+            _writeConnectorMock.Verify(_ => _.DoCreateOrReplaceDigitalTwinAsync(It.IsAny<BasicDigitalTwin>()),
+                Times.Once());
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_no_Relationships_for_minimal_Property()
+        {
+            var minimalProperty = new Property(DataTypeDefXsd.Boolean);
+            await _objectUnderTest.CreateSubmodelElement(minimalProperty);
+
+            _writeBaseMock.Verify(_ => _.AddHasDataSpecification(It.IsAny<string>(), It.IsAny<IHasDataSpecification>()), Times.Never);
+            _writeBaseMock.Verify(_ => _.AddQualifiableRelations(It.IsAny<string>(), It.IsAny<List<Qualifier>>()),
+                Times.Never);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), It.IsAny<string>()),
+                Times.Never);
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_SmeRelationships_for_full_Property()
+        {
+            var fullProperty = new Property(DataTypeDefXsd.Boolean,
+                qualifiers: new List<Qualifier>() { new Qualifier("testType", DataTypeDefXsd.Boolean) },
+                semanticId: new Reference(ReferenceTypes.GlobalReference, new List<Key>()),
+                embeddedDataSpecifications: new List<EmbeddedDataSpecification>()
+                {
+                    new EmbeddedDataSpecification(
+                        new Reference(ReferenceTypes.GlobalReference,
+                            new List<Key>()),new DataSpecificationIec61360(new List<LangString>()))
+                });
+
+
+            await _objectUnderTest.CreateSubmodelElement(fullProperty);
+
+            _writeBaseMock.Verify(_ => _.AddHasDataSpecification(It.IsAny<string>(), It.IsAny<IHasDataSpecification>())
+            , Times.Once);
+            _writeBaseMock.Verify(_ => _.AddQualifiableRelations(It.IsAny<string>(), It.IsAny<List<Qualifier>>()),
+            Times.Once);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), "semanticId"),
+            Times.Once);
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_valueId_if_present()
+        {
+            var propertyWithValueId = new Property(DataTypeDefXsd.Boolean,
+                valueId: new Reference(ReferenceTypes.GlobalReference, new List<Key>()));
+
+            await _objectUnderTest.CreateSubmodelElement(propertyWithValueId);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), "valueId"), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_Creates_SubmodelElementCollection()
+        {
+            var minimalSubmodelElementCollection = new SubmodelElementCollection();
+            await _objectUnderTest.CreateSubmodelElement(minimalSubmodelElementCollection);
+
+            _writeConnectorMock.Verify(_ => _.DoCreateOrReplaceDigitalTwinAsync(It.IsAny<BasicDigitalTwin>()),
+                Times.Once());
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_no_Relationships_for_minimal_SubmodelElement()
+        {
+            var minimalSubmodelElementCollection = new SubmodelElementCollection();
+            await _objectUnderTest.CreateSubmodelElement(minimalSubmodelElementCollection);
+
+            _writeBaseMock.Verify(_ => _.AddHasDataSpecification(It.IsAny<string>(), It.IsAny<IHasDataSpecification>()),
+                Times.Never);
+            _writeBaseMock.Verify(_ => _.AddQualifiableRelations(It.IsAny<string>(), It.IsAny<List<Qualifier>>()),
+                Times.Never);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), It.IsAny<string>()),
+                Times.Never);
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_SmeRelationships_for_full_SubmodelElementCollection()
+        {
+            var fullSmeCollection = new SubmodelElementCollection(
+                qualifiers: new List<Qualifier>() { new Qualifier("testType", DataTypeDefXsd.Boolean) },
+                semanticId: new Reference(ReferenceTypes.GlobalReference, new List<Key>()),
+                embeddedDataSpecifications: new List<EmbeddedDataSpecification>()
+                {
+                    new EmbeddedDataSpecification(
+                        new Reference(ReferenceTypes.GlobalReference,
+                            new List<Key>()), new DataSpecificationIec61360(new List<LangString>()))
+                });
+
+            await _objectUnderTest.CreateSubmodelElement(fullSmeCollection);
+
+            _writeBaseMock.Verify(_ => _.AddHasDataSpecification(It.IsAny<string>(), It.IsAny<IHasDataSpecification>())
+                , Times.Once);
+            _writeBaseMock.Verify(_ => _.AddQualifiableRelations(It.IsAny<string>(), It.IsAny<List<Qualifier>>()),
+                Times.Once);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), "semanticId"),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_creates_two_twins_for_two_contained_SubmodelElements()
+        {
+            var SmeCollectionWithSubmodelElements = new SubmodelElementCollection(
+                value: new List<ISubmodelElement>()
+                    { new Property(DataTypeDefXsd.Boolean), new SubmodelElementCollection() });
+            await _objectUnderTest.CreateSubmodelElement(SmeCollectionWithSubmodelElements);
+
+            _writeConnectorMock.Verify(_ => _.DoCreateOrReplaceDigitalTwinAsync(It.IsAny<BasicDigitalTwin>()),
+                Times.Exactly(3));
+            _writeConnectorMock.Verify(_ => _.DoCreateOrReplaceRelationshipAsync("testSubmodelElementTwinId", "value", It.IsAny<string>()));
+
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_Creates_File()
+        {
+            var minimalFile = new File("testContentType");
+            await _objectUnderTest.CreateSubmodelElement(minimalFile);
+
+            _writeConnectorMock.Verify(_ => _.DoCreateOrReplaceDigitalTwinAsync(It.IsAny<BasicDigitalTwin>()),
+                Times.Once());
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_no_Relationships_for_minimal_File()
+        {
+            var minimalFile = new File("testContentType");
+            await _objectUnderTest.CreateSubmodelElement(minimalFile);
+
+            _writeBaseMock.Verify(_ => _.AddHasDataSpecification(It.IsAny<string>(), It.IsAny<IHasDataSpecification>()),
+                Times.Never);
+            _writeBaseMock.Verify(_ => _.AddQualifiableRelations(It.IsAny<string>(), It.IsAny<List<Qualifier>>()),
+                Times.Never);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), It.IsAny<string>()),
+                Times.Never);
+        }
+
+        [TestMethod]
+        public async Task CreateSubmodelElement_adds_SmeRelationships_for_full_File()
+        {
+            var fullFile = new File("testContentType",
+            qualifiers:
+            new List<Qualifier>() { new Qualifier("testType", DataTypeDefXsd.Boolean) },
+            semanticId:
+            new Reference(ReferenceTypes.GlobalReference, new List<Key>()),
+            embeddedDataSpecifications:
+            new List<EmbeddedDataSpecification>()
+            {
+                new EmbeddedDataSpecification(
+                    new Reference(ReferenceTypes.GlobalReference,
+                        new List<Key>()), new DataSpecificationIec61360(new List<LangString>()))
+            });
+            
+            await _objectUnderTest.CreateSubmodelElement(fullFile);
+
+            _writeBaseMock.Verify(_ => _.AddHasDataSpecification(It.IsAny<string>(), It.IsAny<IHasDataSpecification>())
+                , Times.Once);
+            _writeBaseMock.Verify(_ => _.AddQualifiableRelations(It.IsAny<string>(), It.IsAny<List<Qualifier>>()),
+                Times.Once);
+            _writeBaseMock.Verify(_ => _.AddReference(It.IsAny<string>(), It.IsAny<Reference>(), "semanticId"),
+                Times.Once);
+
+        }
+
+
+
+    }
+}
---
+++ src/AAS ADT SDK/Connectors/AasWriteSubmodelElements.cs
@@ -0,0 +1,166 @@
+﻿using AasCore.Aas3_0_RC02;
+using Azure.DigitalTwins.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAS.ADT
+{
+    public class AasWriteSubmodelElements : IAasWriteSubmodelElements
+    {
+        private readonly ILogger<AasWriteSubmodelElements> _logger;
+        private readonly IAdtTwinFactory _modelFactory;
+        private readonly IAasWriteConnector _aasWriteConnector;
+        private readonly IAasWriteBase _writeBase;
+
+        public AasWriteSubmodelElements(ILogger<AasWriteSubmodelElements> logger, IAdtTwinFactory modelFactory,
+            IAasWriteConnector aasWriteConnector, IAasWriteBase writeBase)
+        {
+            _logger = logger;
+            _modelFactory = modelFactory;
+            _aasWriteConnector = aasWriteConnector;
+            _writeBase = writeBase;
+        }
+
+
+        public async Task<string> CreateSubmodelElement(ISubmodelElement submodelElement)
+        {
+            if (submodelElement is SubmodelElementCollection)
+            {
+                return await CreateSubmodelElementCollection((SubmodelElementCollection)submodelElement);
+            }
+            else if (submodelElement is Property)
+            {
+                return await CreateProperty((Property)submodelElement);
+            }
+            else if (submodelElement is File)
+            {
+                return await CreateFile((File)submodelElement);
+            }
+
+            //else if (submodelElement.submodelElement.GetType() == typeof(MultiLanguageProperty))
+            //{
+            //    return await CreateMultiLanguageProperty(submodelElement.GetAs<MultiLanguageProperty>());
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(AdminShellV20.Range))
+            //{
+            //    return await CreateRange(submodelElement.GetAs<AdminShellV20.Range>(), processInfo);
+            //}
+
+            //else if (submodelElement.submodelElement.GetType() == typeof(Blob))
+            //{
+            //    return await CreateBlob(submodelElement.GetAs<Blob>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(ReferenceElement))
+            //{
+            //    return await CreateReferenceElement(submodelElement.GetAs<ReferenceElement>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(Capability))
+            //{
+            //    return await CreateCapability(submodelElement.GetAs<Capability>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(RelationshipElement))
+            //{
+            //    return await CreateRelationshipElement(submodelElement.GetAs<RelationshipElement>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(AnnotatedRelationshipElement))
+            //{
+            //    return await CreateAnnotatedRelationshipElement(submodelElement.GetAs<AnnotatedRelationshipElement>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(AdminShellV20.Operation))
+            //{
+            //    return await CreateOperation(submodelElement.GetAs<AdminShellV20.Operation>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(BasicEvent))
+            //{
+            //    return await CreateBasicEvent(submodelElement.GetAs<BasicEvent>(), processInfo);
+            //}
+            //else if (submodelElement.submodelElement.GetType() == typeof(Entity))
+            //{
+            //    return await CreateEntity(submodelElement.GetAs<Entity>(), processInfo);
+            //}
+
+            _logger.LogError(
+                $"CreateSubmodelElement called for unsupported SubmodelElement '{submodelElement.GetType()}'");
+
+            return null;
+        }
+
+        private async Task<string> CreateSubmodelElementCollection(SubmodelElementCollection submodelElementCollection)
+        {
+            _logger.LogInformation($"Now creating submodel element collection '{submodelElementCollection.IdShort}'");
+
+            // Start by creating a twin for the Submodel Element collection
+            var twin = _modelFactory.GetTwin(submodelElementCollection);
+
+            await _aasWriteConnector.DoCreateOrReplaceDigitalTwinAsync(twin);
+
+            await AddSubmodelElementRelationships(twin.Id, submodelElementCollection);
+
+            // Add all submodel elements
+            if (submodelElementCollection.Value != null)
+            {
+                foreach (var submodelElement in submodelElementCollection.Value)
+                {
+                    string submodelElementDtId = await CreateSubmodelElement(submodelElement);
+                    if (submodelElementDtId != null)
+                    {
+                        await _aasWriteConnector.DoCreateOrReplaceRelationshipAsync(twin.Id, "value", submodelElementDtId);
+                    }
+                }
+            }
+
+            return twin.Id;
+        }
+
+        private async Task<string> CreateProperty(Property property)
+        {
+            _logger.LogInformation($"Now creating property '{property.IdShort}'");
+
+            // Start by creating a twin for the Property
+            var twin = _modelFactory.GetTwin(property);
+            
+            if (property.ValueId != null)
+                await _writeBase.AddReference(twin.Id, property.ValueId, "valueId");
+            
+            await _aasWriteConnector.DoCreateOrReplaceDigitalTwinAsync(twin);
+            
+            await AddSubmodelElementRelationships(twin.Id, property);
+
+            return twin.Id;
+        }
+
+        private async Task<string> CreateFile(File file)
+        {
+            _logger.LogInformation($"Now creating file '{file.IdShort}'");
+
+            // Start by creating a twin for the Asset
+            var twin = _modelFactory.GetTwin(file);
+
+            await _aasWriteConnector.DoCreateOrReplaceDigitalTwinAsync(twin);
+
+            await AddSubmodelElementRelationships(twin.Id, file);
+
+            return twin.Id;
+        }
+
+
+        private async Task AddSubmodelElementRelationships(string sourceTwinId, ISubmodelElement submodelElement)
+        {
+            // Create semantic Id
+            if (submodelElement.SemanticId != null)
+            {
+                await _writeBase.AddReference(sourceTwinId, submodelElement.SemanticId, "semanticId");
+            }
+
+            if (submodelElement.EmbeddedDataSpecifications != null)
+                await _writeBase.AddHasDataSpecification(sourceTwinId, submodelElement);
+
+            if (submodelElement.Qualifiers != null)
+                await _writeBase.AddQualifiableRelations(sourceTwinId, submodelElement.Qualifiers);
+        }
+    }
+}
